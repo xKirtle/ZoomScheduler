93734754342232a8ee56392cd485beeafa2f1ad8
xKirtle <xkirtle26@gmail.com>
2021-07-03 23:08:45 +0100

    Added Unschedule UI and preview meeting info in the Unschedule tab

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -20,6 +21,11 @@ namespace ZoomScheduler
             
             Button scheduleMeeting = this.FindControl<Button>("ScheduleMeeting_Button");
             scheduleMeeting.Click += ScheduleMeeting_Button_OnClick;
+
+            ComboBox unscheduleComboBox = this.FindControl<ComboBox>("UnscheduleMeeting_ComboBox");
+            unscheduleComboBox.SelectionChanged += UnscheduleComboBox_OnSelectionChanged;
+            
+            UpdateScheduledMeetings();
         }
 
         private void InitializeComponent()
@@ -53,11 +59,60 @@ namespace ZoomScheduler
                 ScheduleMeetingInvalidArg(meetingDays);
             
             ZoomMeeting.SaveMeeting(meeting);
+            UpdateScheduledMeetings();
+            ClearInputFields();
         }
 
         private void ScheduleMeetingInvalidArg(object? sender)
         {
             //Red border around invalid arg?
+            //Disable schedule button?
+        }
+
+        private void UpdateScheduledMeetings()
+        {
+            ComboBox unscheduleComboBox = this.FindControl<ComboBox>("UnscheduleMeeting_ComboBox");
+            unscheduleComboBox.Items = ZoomMeeting.ReadMeetings();
+        }
+
+        private void ClearInputFields()
+        {
+            TextBox info = this.FindControl<TextBox>("MeetingInfo_TextBox");
+            TextBox id = this.FindControl<TextBox>("MeetingId_TextBox");
+            TextBox password = this.FindControl<TextBox>("MeetingPwd_TextBox");
+            TimePicker time = this.FindControl<TimePicker>("MeetingSelectedTime");
+            ListBox meetingDays = this.FindControl<ListBox>("MeetingDays");
+
+            info.Text = id.Text = password.Text = "";
+            time.SelectedTime = TimeSpan.Zero;
+            meetingDays.SelectedItems = null;
+        }
+        
+        private void UnscheduleComboBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            UpdateScheduleMeetingValue(comboBox.SelectedIndex < 0 ? new ZoomMeeting() : ZoomMeeting.ReadMeetings()[comboBox.SelectedIndex]);
+        }
+
+        private void UpdateScheduleMeetingValue(ZoomMeeting meeting)
+        {
+            TextBox info = this.FindControl<TextBox>("MeetingInfo_TextBox2");
+            TextBox id = this.FindControl<TextBox>("MeetingId_TextBox2");
+            TextBox password = this.FindControl<TextBox>("MeetingPwd_TextBox2");
+            TimePicker time = this.FindControl<TimePicker>("MeetingSelectedTime2");
+            ListBox meetingDays = this.FindControl<ListBox>("MeetingDays2");
+            
+            info.Text = meeting.Name;
+            id.Text = meeting.ID.ToString();
+            password.Text = meeting.Password;
+            time.SelectedTime = meeting.Time;
+
+            List<ListBoxItem> selectedItems = new List<ListBoxItem>();
+            string[] lbNames = {"Mon2", "Tue2", "Wed2", "Thu2", "Fri2", "Sat2", "Sun2"};
+            for (int i = 0; i < lbNames.Length; i++)
+                if (meeting.Days[i] == 1)
+                    selectedItems.Add(this.FindControl<ListBoxItem>(lbNames[i]));
+            meetingDays.SelectedItems = selectedItems;
         }
     }
 }
